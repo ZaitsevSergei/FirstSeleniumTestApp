6bf3e53aef94f055a84c56bd12245a9e21ae2c51
Sergei Zaitsev <Sergei_Zaitsev@epam.com>
2017-12-25 17:41:33 +0300

    Change solution and projects names by convention rules.
    Running setMethods test from framework

---
+++ TestWebUIFramework/SeleniumSetMethods.cs
@@ -0,0 +1,55 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace _2017.EPAM.FirstSeleniumTestApp.TestWebUIFramework
+{
+    public class SeleniumSetMethods
+    {
+        // Enter Text
+        /// <summary>
+        /// Test method to put text into text control
+        /// </summary>
+        /// <param name="attribute">Tag, Id, ClassName, Name</param>
+        /// <param name="attributeValue">value of element</param>
+        /// <param name="testValue">value to test the control</param>
+        public static void EnterText(How attribute, string attributeValue, string testValue)
+        {
+            IWebElement webElement = WebDriverTools.FindElement(attribute, attributeValue);
+            webElement?.SendKeys(testValue);
+        }
+
+        // Click
+        /// <summary>
+        /// Test method on the specified control 
+        /// </summary>
+        /// <param name="attribute">Tag, Id, ClassName, Name</param>
+        /// <param name="attributeValue">value of element</param>
+        /// <param name="testValue">value to test the control</param>
+        public static void Click(How attribute, string attributeValue)
+        {
+            IWebElement webElement = WebDriverTools.FindElement(attribute, attributeValue);
+            webElement?.Click();
+        }
+
+        /// <summary>
+        /// Selects an specific item in specified dropdown list
+        /// </summary>
+        /// <param name="attribute">Tag, Id, ClassName, Name</param>
+        /// <param name="attributeValue">value of element</param>
+        /// <param name="itemToSelect">item to select in dropdown list</param>
+        public static void SelectDropDownListItem(How attribute, string attributeValue, string itemToSelect)
+        {
+            IWebElement dropDownList = WebDriverTools.FindElement(attribute, attributeValue);
+            SelectElement selectDropDownListElement = new SelectElement(dropDownList);
+            selectDropDownListElement.SelectByText(itemToSelect);
+            
+        }
+    }
+}
---
+++ TestWebUIFramework/WebDriverTools.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+
+namespace _2017.EPAM.FirstSeleniumTestApp.TestWebUIFramework
+{
+    public class WebDriverTools
+    {
+        public static IWebDriver WebDriver { get; set; }
+        public static IWebElement FindElement(How attribute, string attributeValue)
+        {
+            try
+            {
+                // Find method from By class with reflection by the name that after will be used if FindElement method of web driver instance
+                var findByMethod = typeof(By).GetMethod(attribute.ToString());
+                // Find the control by the element value and put test string into control
+                return WebDriver.FindElement((By)findByMethod.Invoke(null, new object[] { attributeValue }));
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("One of the params is null");
+            }
+            catch (NullReferenceException)
+            {
+                Console.WriteLine("Element with the Element name: '{0}' doesn't exists", attribute, attributeValue);
+            }
+
+            return null;
+        }
+    }
+}
